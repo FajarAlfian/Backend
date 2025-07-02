3e818183f93287f5d28f43e151f2c057994628ec
FajarAlfian <fajaralfian231@gmail.com>
2025-07-03 00:01:59 +0700

    fix: invoice detail

--- backend/Models/Invoice.cs
+++ backend/Models/Invoice.cs
@@ -13,12 +13,21 @@ namespace DlanguageApi.Models
         public int payment_method_id { get; set; }
         public string payment_method_name { get; set; } = string.Empty;
         public int total_courses { get; set; } = 0;
-
         public bool isPaid { get; set; } = false;
-
+        public DateTime invoice_date { get; set; } = DateTime.UtcNow;
+        public List<InvoiceDetail> detail { get; set; } = new();
         public DateTime created_at { get; set; } = DateTime.UtcNow;
         public DateTime updated_at { get; set; } = DateTime.UtcNow;
 
+    }
+
+    public class InvoiceDetail
+    {
+        public int detail_no { get; set; }
+        public string course_name { get; set; } = string.Empty;
+        public string language { get; set; } = string.Empty;
+        public string schedule { get; set; } = string.Empty;
+        public double price { get; set; }
     }
 
     public class InvoiceCreateRequest
@@ -30,7 +39,7 @@ namespace DlanguageApi.Models
         public int payment_method_id { get; set; }
         [Required]
         public string payment_method_name { get; set; } = string.Empty;
-        
+
     }
     public class InvoiceUpdateRequest
     {
